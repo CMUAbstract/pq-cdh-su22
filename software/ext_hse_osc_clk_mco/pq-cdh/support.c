13ad7f9a6775364f9cb6230f0d8219a0a59d45ba
AnandAlok0807 <91696786+AnandAlok0807@users.noreply.github.com>
2022-10-03 10:09:53 -0400

    support file

---
+++ software/ext_hse_osc_clk_mco/pq-cdh/support.c
@@ -0,0 +1,79 @@
+// support.c
+// Tartan Artibeus CDH board UART support implementation file
+//
+// Written by Bradley Denby
+// Other contributors: None
+//
+// See the top-level LICENSE file for the license.
+
+// Standard library
+#include <stdint.h>                 // uint8_t
+
+// libopencm3 library
+#include <libopencm3/cm3/scb.h>     // SCB_VTOR
+#include <libopencm3/stm32/flash.h> // used in init_clock
+#include <libopencm3/stm32/gpio.h>  // used in init_gpio
+#include <libopencm3/stm32/rcc.h>   // used in init_clock
+#include <libopencm3/stm32/usart.h> // used in init_uart
+
+// UART support library
+#include </home/abstract/git-repos/pq-cdh-su22-master/software/ext_hse_osc_clk_mco/pq-cdh/support.h>             // Header file
+
+// Initialization functions
+
+void init_clock(void) {
+  //rcc_osc_on(RCC_HSE);                    // 16 MHz internal RC oscillator
+  //rcc_wait_for_osc_ready(RCC_HSE);        // Wait until oscillator is ready
+  rcc_set_sysclk_source(RCC_CFGR_SW_HSE); // Sets sysclk source for RTOS
+  rcc_set_hpre(RCC_CFGR_HPRE_NODIV);        // AHB at 80 MHz (80 MHz max.)
+  rcc_set_ppre1(RCC_CFGR_PPRE_DIV2);       // APB1 at 40 MHz (80 MHz max.)
+  rcc_set_ppre2(RCC_CFGR_PPRE_NODIV);      // APB2 at 80 MHz (80 MHz max.)
+  //flash_prefetch_enable();                  // Enable instr prefetch buffer
+  flash_set_ws(FLASH_ACR_LATENCY_4WS);      // RM0351: 4 WS for 80 MHz
+  //flash_dcache_enable();                    // Enable data cache
+  //flash_icache_enable();                    // Enable instruction cache
+  rcc_set_main_pll(                         // Setup 80 MHz clock
+   RCC_PLLCFGR_PLLSRC_HSE,                // PLL clock source
+   4,                                       // PLL VCO division factor
+   40,                                      // PLL VCO multiplication factor
+   0,                                       // PLL P clk output division factor
+   0,                                       // PLL Q clk output division factor
+   RCC_PLLCFGR_PLLR_DIV2                    // PLL sysclk output division factor
+  ); // 16MHz/4 = 4MHz; 4MHz*40 = 160MHz VCO; 160MHz/2 = 80MHz PLL
+  /*rcc_osc_on(RCC_PLL);                      // 80 MHz PLL
+  rcc_wait_for_osc_ready(RCC_PLL);          // Wait until PLL is ready
+  rcc_set_sysclk_source(RCC_CFGR_SW_PLL);   // Sets sysclk source for RTOS
+  rcc_wait_for_sysclk_status(RCC_PLL);*/
+
+  rcc_osc_on(RCC_HSE);                    // 16 MHz internal RC oscillator
+  rcc_wait_for_osc_ready(RCC_HSE);        // Wait until oscillator is ready
+  //rcc_osc_on(RCC_PLL);                      // 80 MHz PLL
+  //rcc_wait_for_osc_ready(RCC_PLL);          // Wait until PLL is ready
+
+  rcc_ahb_frequency = 80000000;//160000000;//80000000;
+  rcc_apb1_frequency = 40000000;
+  rcc_apb2_frequency = 80000000;
+}
+
+void init_led(void) {
+  rcc_periph_clock_enable(RCC_GPIOC);
+  gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO10);
+  gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
+  gpio_set(GPIOC, GPIO10);
+  gpio_clear(GPIOC, GPIO12);
+}
+
+void init_uart(void) {
+  rcc_periph_clock_enable(RCC_GPIOA);
+  rcc_periph_clock_enable(RCC_USART1);
+  gpio_mode_setup(GPIOA,GPIO_MODE_AF,GPIO_PUPD_NONE,GPIO9|GPIO10);
+  gpio_set_af(GPIOA,GPIO_AF7,GPIO9);  // USART1_TX is alternate function 7
+  gpio_set_af(GPIOA,GPIO_AF7,GPIO10); // USART1_RX is alternate function 7
+  usart_set_baudrate(USART1,115200);
+  usart_set_databits(USART1,8);
+  usart_set_stopbits(USART1,USART_STOPBITS_1);
+  usart_set_mode(USART1,USART_MODE_TX_RX);
+  usart_set_parity(USART1,USART_PARITY_NONE);
+  usart_set_flow_control(USART1,USART_FLOWCONTROL_NONE);
+  usart_enable(USART1);
+}
