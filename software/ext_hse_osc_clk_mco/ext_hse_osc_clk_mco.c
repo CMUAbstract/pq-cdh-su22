395de340061fc947d38dbbbb0cd5d3f1f3fdc229
AnandAlok0807 <91696786+AnandAlok0807@users.noreply.github.com>
2022-10-30 19:36:03 -0400

    HSE derived PLL clock output 100 MHz

--- software/ext_hse_osc_clk_mco/ext_hse_osc_clk_mco.c
+++ software/ext_hse_osc_clk_mco/ext_hse_osc_clk_mco.c
@@ -14,43 +14,18 @@ int main(void) {
 
   init_clock();
 
-  //GPIO_InitTypeDef GPIO_InitStructure;
-
-  //RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+  
   rcc_periph_clock_enable(RCC_GPIOA);
 
   /* Output clock on MCO pin ---------------------------------------------*/
   gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO8);
-  gpio_set_output_options(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO8);
+  gpio_set_output_options(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, GPIO8);
   gpio_set(GPIOA, GPIO8);
-  
-  
-  /*
-  GPIO_MODE_AF : GPIO alternate function ( Analog/Digital)
- gpio_set_output_options 
- # Parameters
-[in]	gpioport	Unsigned int32. Port identifier GPIO Port IDs
-[in]	otype	Unsigned int8. Pin output type GPIO Output Pin Driver Type
-  GPIO_OTYPE_PP, GPIO_OTYPE_D   PushPull. OpenDrain
-[in]	speed	Unsigned int8. Pin speed GPIO Output Pin Speed
-   #define 	GPIO_OSPEED_LOW   0x0 
-  #define 	GPIO_OSPEED_MED   0x1
-  #define 	GPIO_OSPEED_HIGH   0x2
-  #define 	GPIO_OSPEED_VERYHIGH   0x3
-  #define 	GPIO_OSPEED_2MHZ   0x0
-  #define 	GPIO_OSPEED_25MHZ   0x1
-  #define 	GPIO_OSPEED_50MHZ   0x2
-  #define 	GPIO_OSPEED_100MHZ   0x3
-[in]	gpios	Unsigned int16. Pin identifiers GPIO Pin Identifiers 
-If multiple pins are to be set, use bitwise OR '|' to separate them
-  
-  
-  */
  
-  //RCC_MCOConfig(RCC_MCOSource_PLLCLK_Div2); // Put on MCO pin the: System clock selected
-  //rcc_set_mco(RCC_CFGR_MCO_PLL); //for PLL clk test
-  rcc_set_mco(RCC_CFGR_MCO_HSE);
+  rcc_set_mco(RCC_CFGR_MCO_PLL); //for PLL clk test : 100 MHz
+  //rcc_set_mco(RCC_CFGR_MCO_HSE);  // for checking HSE output : 16 MHz
 
+  // LED pin enable for debugging
   rcc_periph_clock_enable(RCC_GPIOC);
   gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO10);
   gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
