dbf81f22b56b824a2664051996a46a8ceb57a164
AnandAlok0807 <91696786+AnandAlok0807@users.noreply.github.com>
2022-10-30 19:41:09 -0400

    internal HSI configuration

--- software/int_pll_clk_mco_test/pq-cdh/support.c
+++ software/int_pll_clk_mco_test/pq-cdh/support.c
@@ -22,17 +22,24 @@
 // Initialization functions
 
 void init_clock(void) {
-  rcc_set_sysclk_source(RCC_CFGR_SW_PLL); // Sets sysclk source for RTOS
+
+  rcc_osc_on(RCC_HSI16);                      // 80 MHz PLL
+  rcc_wait_for_osc_ready(RCC_HSI16);          // Wait until PLL is ready  
+
   rcc_set_hpre(RCC_CFGR_HPRE_NODIV);        // AHB at 80 MHz (80 MHz max.)
   rcc_set_ppre1(RCC_CFGR_PPRE_DIV2);       // APB1 at 40 MHz (80 MHz max.)
   rcc_set_ppre2(RCC_CFGR_PPRE_NODIV);      // APB2 at 80 MHz (80 MHz max.)
+
+  rcc_osc_off(RCC_PLL);
+
   rcc_set_main_pll(                         // Setup 80 MHz clock
    RCC_PLLCFGR_PLLSRC_HSI16,                // PLL clock source
-   4,                                       // PLL VCO division factor
+   16,                                       // PLL VCO division factor
    40,                                      // PLL VCO multiplication factor
    0,                                       // PLL P clk output division factor
    0,                                       // PLL Q clk output division factor
-   RCC_PLLCFGR_PLLR_DIV2                    // PLL sysclk output division factor
+   RCC_PLLCFGR_PLLR_DIV4                    // PLL sysclk output division factor
+   //RCC_PLLCFGR_PLLR_DIV8  
   );
   // 16MHz/4 = 4MHz; 4MHz*40 = 160MHz VCO; 160MHz/2 = 80MHz PLL
   // 16MHz/4 = 4MHz; 4MHz*16 = 64MHz VCO; 64MHz/2 = 32MHz PLL
@@ -40,12 +47,20 @@ void init_clock(void) {
   // 16MHz/4 = 4MHz; 4MHz*4 = 16MHz VCO; 16MHz/2 = 8MHz PLL
   // 16MHz/4 = 4MHz; 4MHz*1 = 4MHz VCO; 2MHz/2 = 2MHz PLL
   
-  rcc_osc_on(RCC_HSI16);                      // 80 MHz PLL
-  rcc_wait_for_osc_ready(RCC_HSI16);          // Wait until PLL is ready  
-
+  //Division factor
+  //Path : libopencm3/include/libopencm3/stm32/l4
+  /*
+   RCC_PLLCFGR_PLLR_DIV2		0   =>8MHz
+   RCC_PLLCFGR_PLLR_DIV4		1   =>4MHz
+   RCC_PLLCFGR_PLLR_DIV6		2  => 2MHz
+   RCC_PLLCFGR_PLLR_DIV8		3
+  */
   rcc_osc_on(RCC_PLL);                      // 80 MHz PLL
   rcc_wait_for_osc_ready(RCC_PLL);          // Wait until PLL is ready
-  
+
+  rcc_set_sysclk_source(RCC_CFGR_SW_PLL); 
+  rcc_wait_for_sysclk_status(RCC_PLL);  
+
   rcc_ahb_frequency = 80000000;//160000000;//80000000;
   rcc_apb1_frequency = 40000000;
   rcc_apb2_frequency = 80000000;
@@ -73,3 +88,35 @@ void init_uart(void) {
   usart_set_flow_control(USART1,USART_FLOWCONTROL_NONE);
   usart_enable(USART1);
 }
+
+
+/*
+void init_clock(void) {
+  rcc_set_sysclk_source(RCC_CFGR_SW_PLL); // Sets sysclk source for RTOS
+  rcc_set_hpre(RCC_CFGR_HPRE_NODIV);        // AHB at 80 MHz (80 MHz max.)
+  rcc_set_ppre1(RCC_CFGR_PPRE_DIV2);       // APB1 at 40 MHz (80 MHz max.)
+  rcc_set_ppre2(RCC_CFGR_PPRE_NODIV);      // APB2 at 80 MHz (80 MHz max.)
+  rcc_set_main_pll(                         // Setup 80 MHz clock
+   RCC_PLLCFGR_PLLSRC_HSI16,                // PLL clock source
+   40,                                       // PLL VCO division factor
+   4,                                      // PLL VCO multiplication factor
+   0,                                       // PLL P clk output division factor
+   0,                                       // PLL Q clk output division factor
+   RCC_PLLCFGR_PLLR_DIV2                    // PLL sysclk output division factor
+  );
+  // 16MHz/4 = 4MHz; 4MHz*40 = 160MHz VCO; 160MHz/2 = 80MHz PLL
+  // 16MHz/4 = 4MHz; 4MHz*16 = 64MHz VCO; 64MHz/2 = 32MHz PLL
+  // 16MHz/4 = 4MHz; 4MHz*8 = 32MHz VCO; 32MHz/2 = 16MHz PLL
+  // 16MHz/4 = 4MHz; 4MHz*4 = 16MHz VCO; 16MHz/2 = 8MHz PLL
+  // 16MHz/4 = 4MHz; 4MHz*1 = 4MHz VCO; 2MHz/2 = 2MHz PLL
+  
+  rcc_osc_on(RCC_HSI16);                      // 80 MHz PLL
+  rcc_wait_for_osc_ready(RCC_HSI16);          // Wait until PLL is ready  
+
+  rcc_osc_on(RCC_PLL);                      // 80 MHz PLL
+  rcc_wait_for_osc_ready(RCC_PLL);          // Wait until PLL is ready
+  
+  rcc_ahb_frequency = 80000000;//160000000;//80000000;
+  rcc_apb1_frequency = 40000000;
+  rcc_apb2_frequency = 80000000;
+}*/
